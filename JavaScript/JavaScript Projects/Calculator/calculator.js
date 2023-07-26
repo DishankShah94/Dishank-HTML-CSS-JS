7129962d652f987d4c6aa5daa8ad007182e35ffd
DishankShah94 <dishankshah94@gmail.com>
2023-07-26 13:40:57 +0530

    Calculator JS
    Calculator JS

--- JavaScript/JavaScript Projects/Calculator/calculator.js
+++ JavaScript/JavaScript Projects/Calculator/calculator.js
@@ -1,10 +1,26 @@
 let string = "";
+let memory = 0;
+let result = 0;
 let buttons = document.querySelectorAll(".button");
+function updateDisplay() {
+    document.querySelector("input").value = result;
+}
+
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == "=") {
             string = eval(string);
             document.querySelector("input").value = string;
+        }
+        else if (e.target.innerHTML == "M+") {
+            string = eval(string);
+            memory += string;
+            document.querySelector("input").value = memory;
+        }
+        else if (e.target.innerHTML == "M-") {
+            string = eval(string);
+            memory -= string;
+            document.querySelector("input").value = memory;
         }
         else if (e.target.innerHTML == "C") {
             string = "";
